dc61ff556283d23f616bc9cc7db46f05309e5492
Akshay Patil <patil.akshay11235@gmail.com>
2024-09-18 18:33:53 +0200

    Update main.f90

--- src/main.f90
+++ src/main.f90
@@ -25,7 +25,7 @@ program sdfgenerator
     real(dp) :: t, u, v 
     ! Data for distance
     real(dp) :: setsign, sdfresolution
-    real(dp) :: distance_v1, testdata, previoustestdata    
+    real(dp) :: distance, testdata, previoustestdata    
     ! Iterator
     integer :: i, j, k, faceid, cid, point_tracker, vertexid, pbarwidth
     ! Time loggers
@@ -35,9 +35,9 @@ program sdfgenerator
     !
     ! Log CPU time at the start
     call cpu_time(time1)
-    ! Read the input file
+    ! Read the input file (all ranks read inputfile)
     call read_inputfile()
-    ! Read the grid
+    ! Read the grid (all ranks read the grid)
     call read_cans_grid()
     ! Allocate memory for the grid
     allocate(pointinside(nx,ny,nz))
@@ -69,29 +69,7 @@ program sdfgenerator
     print *, "*** 1st - Pass: Distance calculation ***"
     call cpu_time(time1)        ! Log CPU time at the start of the operation
     ! First compute the scalar distance
-    do k=sz,ez
-        call show_progress((k-sz),(ez-sz),pbarwidth)
-        do j=sy,ey
-            do i=sx,ex
-                querypoint = (/x(i),y(j),z(k)/)
-                previoustestdata = 1e10
-                testdata = 1e10
-                do vertexid=1,num_vertices
-                    ! Each face has three vertices
-                    ! Compute the distance
-                    distance_v1 = sqrt( (x(i) - vertices(1,vertexid))*(x(i) - vertices(1,vertexid)) &
-                                    +   (y(j) - vertices(2,vertexid))*(y(j) - vertices(2,vertexid)) &
-                                    +   (z(k) - vertices(3,vertexid))*(z(k) - vertices(3,vertexid)) & 
-                                        )                
-                    testdata = min(distance_v1,previoustestdata)
-                    ! Assign current testdata to previoustestdata
-                    previoustestdata = testdata
-                end do
-                pointinside(i,j,k) = testdata                
-            end do
-        end do            
-    end do   
-
+    call compute_scalar_distance(sx,ex,sy,ey,sz,ez,num_vertices,vertices,pointinside)  
     print *, "*** 2nd - Pass: Narrow band tagging"
     ! Check indices for where values are < sdfresolution*min(dx,dy,dz)
     narrowmask = pointinside < sdfresolution*min(dx,dy,dz(2))
@@ -99,46 +77,10 @@ program sdfgenerator
     print *, "Total number of narrow band point: ", numberofnarrowpoints, "of", (ex-sx)*(ey-sy)*(ez-sz), "total points."
     ! Allocate the values and indices to the narrowbandindices array
     allocate(narrowbandindices(3,numberofnarrowpoints))
-    point_tracker = 1
-    do k=sz,ez
-        call show_progress((k-sz),(ez-sz),pbarwidth)
-        do j=sy,ey
-            do i=sx,ex
-                if(narrowmask(i,j,k) .eqv. .True.) then
-                    narrowbandindices(:,point_tracker) = (/i,j,k/)
-                    point_tracker = point_tracker + 1
-                endif
-            end do
-        end do
-    end do
+    call tag_narrowband_points(sx,ex,sy,ey,sz,ez,narrowmask,narrowbandindices)
     ! Now compute the ray intersection only for the narrowband points
-    print *, "*** 3rd - Pass: Computing the sign for the distance ***"
-    ! Reset 
-    do point_tracker=1,numberofnarrowpoints
-        call show_progress(point_tracker,numberofnarrowpoints,pbarwidth)
-        querypoint = narrowbandindices(:,point_tracker)
-        intersection_count = 0
-        previoustestdata = 1e10
-        testdata = 1e10
-        do cid=1,numberofrays                                       
-            do faceid=1,num_faces
-                ! Each face has three vertices
-                v1 = faces(1,faceid)
-                v2 = faces(2,faceid)
-                v3 = faces(3,faceid)
-                call ray_triangle_intersection(querypoint, directions(:,cid), vertices(:,v1), vertices(:,v2), vertices(:,v3), t, u, v, intersect)        
-                if (intersect) then
-                    intersection_count = intersection_count + 1
-                end if        
-            end do
-        end do
-        if(mod(intersection_count,2)>0) then
-            setsign = -1.0
-        else
-            setsign = 1.0
-        endif
-        pointinside(i,j,k) = setsign*pointinside(i,j,k)                          
-    end do
+    print *, "*** 3rd - Pass: Computing the sign for the distance ***" 
+    call get_signed_distance(num_faces,numberofnarrowpoints,faces,vertices,directions,narrowbandindices,pointinside)
     ! Log CPU time at the end of the 
     call cpu_time(time2)
     print *, "*** Signed-Distance-Field (SDF) computed in ",time2-time1, "seconds ***"
@@ -146,14 +88,15 @@ program sdfgenerator
     print *, "*** Writing SDF to file ***"
     call cpu_time(time1)
     ! Write the mask to file
-    open(unit=10, file='mask.bin', status='replace', form='unformatted')
+    open(unit=10, file='data/mask.bin', status='replace', form='unformatted')
     write(10) pointinside
     close(10)
-    ! Write debugging index files
-    open(unit=10, file='mask_index.bin', status='replace', form='unformatted')
-    write(10) narrowbandindices
-    close(10)
-    call cpu_time(time2)
+    ! Write points where SDF is tagged with a sign
+    open(unit=11, file='data/narrowbandpoints.bin', status='replace', form='unformatted')
+    write(11) numberofnarrowpoints
+    write(11) narrowbandindices
+    close(11)
+
     elapsedTime = elapsedTime + (time2-time1)
     print *, "*** Finished in ",elapsedTime, "seconds ***"
 
